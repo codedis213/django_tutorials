fb928f587445c352910427acf513671fd3d534d0
codedis213 <codedish213@gmail.com>
2015-06-22 16:16:48 +0530

    adding bunch of  choice to polls in admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from polls.models import Poll, Choice
+from polls.models import Choice, Poll
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Question',               {'fields': ['question']}),
+        (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-
-
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
